0c5a1aa929ae45e0ec5bad23c908a1252f13ef96
Elyass <ilyassgridech0@gmail.com>
2025-12-06 13:18:48 +0100

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
+  base:process.env.VITE_BASE_PATH || "/Parfumerie-Jnane-Meknes",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
